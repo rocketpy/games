8aef60bd8019d1761ffa4c2626d46b7be7238470
rocketpy <dimitr.ushakov@gmail.com>
2020-11-01 23:55:32 +0200

    updated

--- sea_war.py
+++ sea_war.py
@@ -3,3 +3,15 @@ from tkinter import messagebox
 
 
 tk = Tk()
+app_run = True
+
+size_canv_x = 800
+size_canv_y = 800
+
+def closing():
+    pass
+  
+tk.title("Sea  Buttle")
+tk.resizable(0, 0)
+
+  
