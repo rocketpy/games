496b51951bf5089f981fa0827b74942d94aea3fe
rocketpy <dimitr.ushakov@gmail.com>
2020-11-11 17:56:05 +0200

    updated

--- pygame_tutor.py
+++ pygame_tutor.py
@@ -15,4 +15,7 @@ vel =  #  velocity
 run = True
 while run:
     pygame.time.delay(100)
-    
+    for event in pygame.event.get()
+        if even.type == pygame.QUIT:
+            run = False
+pygame.quit()            
