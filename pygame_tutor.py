5269d1e4bbe08891d93e1a5ec5938402b43b3cfc
rocketpy <dimitr.ushakov@gmail.com>
2020-11-11 18:03:59 +0200

    updated

--- pygame_tutor.py
+++ pygame_tutor.py
@@ -18,4 +18,18 @@ while run:
     for event in pygame.event.get()
         if even.type == pygame.QUIT:
             run = False
+            
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:    
+        y += vel
+    pygame.draw.rect(new_window, (255, 0, 0), (x, y, width, height))        
+    pygame.display.update()
+    
 pygame.quit()            
