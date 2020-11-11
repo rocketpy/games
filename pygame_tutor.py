6e14d0aac297410982d5f41c74a8d0233b769ddb
rocketpy <dimitr.ushakov@gmail.com>
2020-11-11 17:53:30 +0200

    updated

--- pygame_tutor.py
+++ pygame_tutor.py
@@ -1,1 +1,18 @@
+import pygame
 
+
+pygame.init()
+
+new_window = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("New window")  # Title
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel =  #  velocity
+
+run = True
+while run:
+    pygame.time.delay(100)
+    
